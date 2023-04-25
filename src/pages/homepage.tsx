0d6c6d7316241fa71c9cfc74a44ad8ddcb84a663
fariscoric <faris.coric@gmail.com>
2023-04-25 16:35:02 +0200

    color-fix

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -81,11 +81,11 @@ const HomePage = () => {
             <div className='relative'>
             <div className="flex flex-row overflow-x-scroll scroll scroll-smooth sliderClass" id='slider'>
                 {/* HORIZONTAL SCROLL BUTTONS */}
-            <button type="button" className="text-white absolute top-1/2 right-5 bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-400" onClick={sliderRight}>
+            <button type="button" className="text-white absolute top-1/2 right-5 bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-red-500 dark:hover:bg-red-700 dark:focus:ring-red-400" onClick={sliderRight}>
             <svg aria-hidden="true" className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             <span className="sr-only">Icon description</span>
             </button>
-            <button type="button" className="text-white absolute top-1/2 left-5 bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-400" onClick={sliderLeft}>
+            <button type="button" className="text-white absolute top-1/2 left-5 bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-red-500 dark:hover:bg-red-700 dark:focus:ring-red-400" onClick={sliderLeft}>
             <svg aria-hidden="true" className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 010 2H5.414l4.293 4.293a1 1 0 010 1.414z" clip-rule="evenodd"></path></svg>
             <span className="sr-only">Icon description</span>
             </button>
